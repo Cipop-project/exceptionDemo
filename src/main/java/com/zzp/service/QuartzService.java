745d68d64af7e2e55d9b25a5b498954a74b45e30
Yang Naihua <naihua.yang@gmail.com>
2018-09-24 10:57:28 +0200

    fix for mysql

--- src/main/java/com/zzp/service/QuartzService.java
+++ src/main/java/com/zzp/service/QuartzService.java
@@ -15,21 +15,21 @@ import java.util.Random;
 public class QuartzService {
 
 //    每分钟启动
-    @Scheduled(cron = "0 0/1 * * * ?")
+//    @Scheduled(cron = "0 0/1 * * * ?")
     public void timerToNow(){
 
         System.out.println("now time:" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
 
     }
 //    项目运行后的每5秒一次
-    @Scheduled(fixedRate = 5000)
+//    @Scheduled(fixedRate = 5000)
     public void timerToZZP(){
 
         System.out.println("ZZP:" + new Random().nextLong() + new SimpleDateFormat("HH:mm:ss").format(new Date()));
 
     }
 //    项目启动后每50秒启动
-    @Scheduled(fixedDelay = 50000)
+//    @Scheduled(fixedDelay = 50000)
     public void timerToReportCount(){
         for (int i = 0; i < 10; i++){
 
@@ -37,7 +37,7 @@ public class QuartzService {
         }
     }
 //    第一次启动在项目启动后延迟50秒开始，之后每6秒执行一次
-    @Scheduled(initialDelay = 50000,fixedRate = 6000)
+//    @Scheduled(initialDelay = 50000,fixedRate = 6000)
     public void timerToReport(){
         for (int i = 0; i < 10; i++){
 
